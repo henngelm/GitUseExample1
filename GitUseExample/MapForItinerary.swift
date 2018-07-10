d8b6dabeec3044e39918fb92d995edcd04e44309
Henngel Mojica <henngelm@icloud.com>
2018-07-10 00:23:43 -0400

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Henngel Mojica on 7/10/18.
+//  Copyright © 2018 Henngel Mojica. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
